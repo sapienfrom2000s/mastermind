d948360b62bc6f4b6436116c616f3fa7464c8352
brickwall <sk292207@gmail.com>
2022-12-20 18:42:36 +0530

    Refactor: remove data clump

--- feedback.rb
+++ feedback.rb
@@ -1,34 +1,32 @@
 class Feedback
-  attr_reader :pegs
   FEEDBACK_STRING_MAX_INDEX = 3
 
   def hint(secretkey, triedkey)
-    secretkey = secretkey.split('')
-    triedkey = triedkey.split('')
-    @pegs = []
-    @pegs = black_pegs(secretkey, triedkey) +\
-            white_pegs(secretkey, triedkey)
+    @secretkey = secretkey.split('')
+    @triedkey = triedkey.split('')
+    pegs = (black_pegs + \
+            white_pegs).join
   end
 
-  #'*','?' is used as a trick so that each count is unique
-  def black_pegs(secretkey, triedkey)
+  # '*','?' is used as a trick so that each count is unique
+  def black_pegs
     pegs = []
     0.upto(FEEDBACK_STRING_MAX_INDEX) do |index|
-      if secretkey[index] == triedkey[index]
-        pegs.push('B')
-        secretkey[index] = '*'
-        triedkey[index] = '?'
-      end
+      next unless @secretkey[index] == @triedkey[index]
+
+      pegs.push('B')
+      @secretkey[index] = '*'
+      @triedkey[index] = '?'
     end
     pegs
   end
 
-  def white_pegs(secretkey, triedkey)
+  def white_pegs
     pegs = []
-    triedkey.each do |element|
-      if index = secretkey.find_index(element)
+    @triedkey.each do |element|
+      if index = @secretkey.find_index(element)
         pegs.push('W')
-        secretkey[index] = '*'
+        @secretkey[index] = '*'
       end
     end
     pegs
